110b7b0fedb5dba84fa130b782fc4f2f7a9343cc
amarantec <marcoscarvalhop@outlook.com>
2025-09-01 22:11:23 -0300

    Validate inputs

--- lib/hasty/contacts/contact.ex
+++ lib/hasty/contacts/contact.ex
@@ -13,11 +13,20 @@ defmodule Hasty.Contacts.Contact do
     timestamps(type: :utc_datetime)
   end
 
+  @digits_regex ~r/^\d+$/
   @doc false
   def changeset(contact, attrs, user_scope) do
     contact
     |> cast(attrs, [:ddi, :ddd, :phone_number])
     |> validate_required([:ddi, :ddd, :phone_number])
+    # only digits
+    |> validate_format(:ddi, @digits_regex, message: "Must contain only digits")
+    |> validate_format(:ddd, @digits_regex, message: "Must contain only digits")
+    |> validate_format(:phone_number, @digits_regex, message: "Must contain only digits")
+    # length
+    |> validate_length(:ddi, min: 1, max: 3)
+    |> validate_length(:ddd, is: 2)
+    |> validate_length(:phone_number, min: 8, max: 9)
     |> put_change(:user_id, user_scope.user.id)
   end
 end
