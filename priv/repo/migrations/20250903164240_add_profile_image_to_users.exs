ed2076e3083bbb62380223cdb6e181e8dd26ab3b
amarantec <marcoscarvalhop@outlook.com>
2025-09-08 15:21:49 -0300

    new field for image profile path

---
+++ priv/repo/migrations/20250903164240_add_profile_image_to_users.exs
@@ -0,0 +1,9 @@
+defmodule Hasty.Repo.Migrations.AddProfileImageToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :profile_image, :string
+    end
+  end
+end
