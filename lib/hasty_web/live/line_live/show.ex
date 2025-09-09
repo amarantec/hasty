95756d1c25a8548a882748d4b8fa430a8b5154f3
amarantec <marcoscarvalhop@outlook.com>
2025-09-09 01:29:32 -0300

    add stop line orders to improve user xp

--- lib/hasty_web/live/line_live/show.ex
+++ lib/hasty_web/live/line_live/show.ex
@@ -2,13 +2,14 @@ defmodule HastyWeb.LineLive.Show do
   use HastyWeb, :live_view 
 
   alias Hasty.Lines
+  alias Hasty.LineStops
 
   @impl true
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
       <.header>
-        Line {@line.id}
+        Line - {@line.name}
         <:subtitle>This is a line record from your database</:subtitle>
         <:actions>
           <.button navigate={~p"/lines"}>
@@ -19,7 +20,38 @@ defmodule HastyWeb.LineLive.Show do
 
       <.list>
         <:item title="Name">{@line.name}</:item>
+        <:item title="Flat Fare">{@line.flat_fare}</:item>
+        <:item title="Base Price">{@line.base_price}</:item>
       </.list>
+
+      <div class="divider" />
+
+      <p>Line Stops Orders</p>
+      <.table
+        id="line_stops"
+        rows={@streams.line_stops}
+        row_click={fn {_id, line_stop} -> JS.navigate(~p"/line-stops/#{line_stop}") end}
+      >
+        <:col :let={{_id, line_stop}} label="Line Stop Order">{line_stop.order}</:col>
+        <:action :let={{_id, line_stop}}>
+          <div class="sr-only">
+            <.link navigate={~p"/line-stops/#{line_stop}"}>Show</.link>
+          </div>
+          <%= if @current_scope && @current_scope.user.role == "admin" do %>
+            <.link navigate={~p"/admin/line-stops/#{line_stop}/edit"}>Edit</.link>
+          <% end %>
+        </:action>
+        <:action :let={{id, line_stop}}>
+          <%= if @current_scope && @current_scope.user.role == "admin" do %>
+            <.link
+              phx-click={JS.push("delete", value: %{id: line_stop.id}) |> hide("##{id}")}
+              data-confirm="Are you sure?"
+            >
+              Delete
+            </.link>
+          <% end %>
+        </:action>
+      </.table>
     </Layouts.app>
     """ 
   end
@@ -29,6 +61,7 @@ defmodule HastyWeb.LineLive.Show do
     {:ok,
       socket
       |> assign(:page_title, "Show Line")
+      |> stream(:line_stops, LineStops.list_line_stops_for_line(id))
       |> assign(:line, Lines.get_line(id))}
   end
 end
