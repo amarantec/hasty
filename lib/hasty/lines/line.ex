a02212f91ca7550b51da7f52a7da857fcdb72f87
amarantec <marcoscarvalhop@outlook.com>
2025-09-09 01:25:25 -0300

    add relation in schema

--- lib/hasty/lines/line.ex
+++ lib/hasty/lines/line.ex
@@ -10,6 +10,7 @@ defmodule Hasty.Lines.Line do
     field :base_price, :decimal
 
     has_many :buses, Hasty.Buses.Bus
+    has_many :line_stops, Hasty.Lines.LineStop
 
     timestamps(type: :utc_datetime)
   end
